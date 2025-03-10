7a1b332a6c4a933781d08079b903a785f5db98e8
benahmed-ayman <ayman.benahmed@student.hepl.be>
2025-03-10 17:04:58 +0100

    cable_changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
-/*faire en sorte que les doux logo de l'ecole devient en couleur lors du survole*/
+const liegeatlas_gris =document.getElementById('liege_atlas_gris');
+const hepl_gris =document.getElementById('hepl_gris')
+
+liegeatlas_gris.addEventListener('mouseenter', () => {
+    liegeatlas_gris.src='./img/liege_atlas_color.svg';
+});
+
+liegeatlas_gris.addEventListener('mouseout', () => {
+    liegeatlas_gris.src='./img/logo_liege_atlas.svg';
+});
+
+
+
+
+hepl_gris.addEventListener('mouseenter', () => {
+    hepl_gris.src='img/logo_hepl_color.svg';
+});
+
+hepl_gris.addEventListener('mouseout', () => {
+    hepl_gris.src='./img/logo_hepl_gris.svg';
+});
+
+
+
+
+
+/*
+
+aussi faire en sorte que les logo defile
+
+const image = document.getElementById("changeableImage");
+const imageSrcDefault = "https://via.placeholder.com/300x200";
+const imageSrcHover = "https://via.placeholder.com/300x200/ff0000/ffffff";
+
+image.addEventListener("mouseover", () => {
+    image.src = imageSrcHover;
+});
+
+image.addEventListener("mouseout", () => {
+    image.src = imageSrcDefault;
+});
+
+*/
